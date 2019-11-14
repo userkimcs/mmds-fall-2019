c4b6c61574febb14b4973917ffc0f650c7a843da
Kim Nguyen <ngkim623.yahoo.com@gmail.com>
2019-11-14 09:07:43 +0700

    Create test.py

---
+++ src/test.py
@@ -0,0 +1,65 @@
+import time
+import redis
+from helpers import get_absolute_links, normalize_url, to_sha1, DatabaseService
+from datetime import datetime
+from newspaper import Article
+from helpers import get_keywords
+import json
+
+
+def get_content(input_url):
+    article = Article(input_url)
+    article.download()
+    article.parse()
+    # add article object (full text, title, date, authors)
+    article_dict = dict()
+    article_dict['url'] = input_url
+    article_dict['text'] = article.text
+    article_dict['title'] = article.title
+    article_dict['authors'] = article.authors
+    keywords = get_keywords(article.text)
+    keywords.extend(get_keywords(article.title))
+
+    return article_dict, keywords
+
+
+if __name__ == "__main__":
+
+    pg = DatabaseService(
+        host="10.255.255.6",
+        user="postgres",
+        password=None,
+        port=5432,
+        database="data"
+    )
+
+    exist_urls = redis.Redis(host='10.255.255.10')
+
+    # load homepages
+    homepages = open("pages.txt").readlines()
+
+    while True:
+
+        for homepage in homepages:
+            homepage = homepage.strip()
+            # send encoded home page to redis anh add to kafka consumer
+            all_urls = get_absolute_links(homepage)
+            for url in all_urls:
+                url = normalize_url(url)
+                encoded_url = to_sha1(url)
+                if not exist_urls.exists(encoded_url):
+                    # add new url to redis and add original to kafka producer
+                    exist_urls.set(encoded_url, 0)
+                    # get content
+                    data, keywords = get_content(url)
+                    # insert
+                    pg.query("INSERT INTO articles(data) VALUES('{}')".format(datetime.now(), json.dumps(data)))
+                    print("INSERT articles")
+                    # insert
+                    time.sleep(0.01)
+
+                    for keyword in keywords:
+                        pg.query("INSERT INTO kw_stream VALUES('{}', '{}')".format(datetime.now(), keyword))
+                        print("Insert {}".format(keyword))
+
+        time.sleep(10)
